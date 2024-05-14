f70760995843c0c866d76e1f2169c77b437f67aa
Edanur <edanurgurgen94@gmail.com>
2024-05-14 10:17:40 +0300

    Add CustomerRepository and AvailableDateRepository - Create CustomerRepository to manage customer data. - Implement AvailableDateRepository for handling available dates.

---
+++ src/main/java/com/edanur/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.edanur.repository;
+
+import com.edanur.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+    Optional<Customer> findByMail (String mail);
+
+    List<Customer> findByName(String name);
+}
